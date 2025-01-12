ba4b4fd7432bdeb2df2fef8f9921390b32bd6050
James Bianca <james.m.bianca@gmail.com>
2025-01-11 21:39:47 -0700

    Assignment 1 initial commit

---
+++ Module1/Assignment1.py
@@ -0,0 +1,37 @@
+#Part 1
+
+# psuedo code:
+# ask user for 2 numbers
+# compute their sum and difference
+# display the output
+
+# gather user input and convert to integer
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+# compute the sum and difference
+addition = num1 + num2
+subtraction = num1 - num2
+
+# display the results
+print(num1,"+",num2,"is",addition)
+print(num1,"-",num2,"is",subtraction)
+
+#Part 2
+
+# psuedo code:
+# ask user for 2 numbers
+# compute their product and quotient
+# display the output
+
+# gather user input and convert to integer
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+# compute the product and quotient
+product = num1 * num2
+quotient = num1 / num2
+
+# display the results
+print(num1,"*",num2,"is",product)
+print(num1,"/",num2,"is",quotient)
