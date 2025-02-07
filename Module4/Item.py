63e6db69d9bd360d77bda6ec7da2b8c582f7694a
jmbiancaData <james.m.bianca@gmail.com>
2025-02-06 21:31:06 -0700

    Added main.py file
    Created the basic functionality called out
    - user enters item name
    - enters price
    - enter quantity

--- Module4/Item.py
+++ Module4/Item.py
@@ -12,5 +12,8 @@ class ItemToPurchase:
         self.item_price = price
         self.item_quantity = quantity
 
-    def print_item_cost(self):
+    def item_total(self):
         return self.item_price * self.item_quantity
+    
+    def print_item_cost(self):
+        print("{} {} @ ${} = {:.2f}".format(self.item_name,self.item_quantity,self.item_price,self.item_total())) 
---
+++ Module4/ProjectMilestone.py
@@ -0,0 +1,23 @@
+from Item import ItemToPurchase
+
+# Item 1
+name = input("Enter the item name: ")
+price = float(input("Enter the item price: "))
+quant = int(input("Enter the item quantity: "))
+
+# Parameterized Constructor
+item1 = ItemToPurchase( name, price, quant ) 
+
+# Item 2
+name = input("Enter the item name: ")
+price = float(input("Enter the item price: "))
+quant = int(input("Enter the item quantity: "))
+
+# Parameterized Constructor
+item2 = ItemToPurchase( name, price, quant )
+
+# Output
+print("Total Cost")
+item1.print_item_cost()
+item2.print_item_cost()
+print('Total: ${:.2f}'.format(item1.item_total() + item2.item_total()))
