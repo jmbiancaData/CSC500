f2e91f78a8b9af37d37b392839588c1881fffe8a
jmbiancaData <james.m.bianca@gmail.com>
2025-02-06 20:54:34 -0700

    Create Item.py
    Initial Commit containing class file

---
+++ Module4/Item.py
@@ -0,0 +1,16 @@
+class ItemToPurchase:
+
+    #default constructor
+    def __init__(self):
+        self.item_name = 'none'
+        self.item_price = 0.0
+        self.item_quantity = 0
+
+    # parameterized constructor
+    def __init__(self,name,price,quantity):
+        self.item_name = name
+        self.item_price = price
+        self.item_quantity = quantity
+
+    def print_item_cost(self):
+        return self.item_price * self.item_quantity
