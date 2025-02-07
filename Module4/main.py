7c70e9db0a600ce4fa840f17a659325e94e799d5
jmbiancaData <james.m.bianca@gmail.com>
2025-02-06 21:36:42 -0700

    Update main.py
    Added main function

--- Module4/main.py
+++ Module4/main.py
@@ -1,23 +1,29 @@
+# Portfolio Milestone
+# import class from our library
 from Item import ItemToPurchase
 
-# Item 1
-name = input("Enter the item name: ")
-price = float(input("Enter the item price: "))
-quant = int(input("Enter the item quantity: "))
+def main():
+    # Item 1
+    name = input("Enter the item name: ")
+    price = float(input("Enter the item price: "))
+    quant = int(input("Enter the item quantity: "))
 
-# Parameterized Constructor
-item1 = ItemToPurchase( name, price, quant ) 
+    # Parameterized Constructor
+    item1 = ItemToPurchase( name, price, quant ) 
 
-# Item 2
-name = input("Enter the item name: ")
-price = float(input("Enter the item price: "))
-quant = int(input("Enter the item quantity: "))
+    # Item 2
+    name = input("Enter the item name: ")
+    price = float(input("Enter the item price: "))
+    quant = int(input("Enter the item quantity: "))
 
-# Parameterized Constructor
-item2 = ItemToPurchase( name, price, quant )
+    # Parameterized Constructor
+    item2 = ItemToPurchase( name, price, quant )
 
-# Output
-print("Total Cost")
-item1.print_item_cost()
-item2.print_item_cost()
-print('Total: ${:.2f}'.format(item1.item_total() + item2.item_total()))
+    # Output
+    print("Total Cost")
+    item1.print_item_cost()
+    item2.print_item_cost()
+    print('Total: ${:.2f}'.format(item1.item_total() + item2.item_total()))
+
+if __name__ == '__main__':
+    main()
